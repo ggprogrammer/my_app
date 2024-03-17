4f109013216892c777a5ec8ef3818677ec50df99
Nikita <vasiliy26105@gmail.com>
2024-03-17 15:36:27 +0300

    profile_temp_version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
     <header className='header'>
         <img className='logo' src='https://steamuserimages-a.akamaihd.net/ugc/2040735491767476405/E4F8C72584384CAA8819DE71985528C50BBD95A6/?imw=512&amp;imh=499&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true'></img>
-        <p>раз два три</p>
     </header>
     );
 }
